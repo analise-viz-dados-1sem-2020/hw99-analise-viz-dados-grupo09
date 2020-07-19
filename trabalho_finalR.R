402a47aea03a012eb2672aea49545b2b946e9918
pedro polastri <pedropolastri94@gmail.com>
2020-07-19 13:58:39 -0300

    Com encoding

--- trabalho_finalR.R
+++ trabalho_finalR.R
@@ -1,4 +1,4 @@
 library(tidyverse)
 
-df <- read.csv2("data/pda-prouni-2019.csv")
+df <- read.csv("pda-prouni-2019.csv", h = T, sep = ";", encoding = "UTF-8")
   #Base de dados a ser utilizada com informações de bolsistas do ProUni
