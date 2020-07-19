928546d729c58080ce3ee2175ac84f3df4c3ab28
pedro polastri <pedropolastri94@gmail.com>
2020-07-19 14:04:42 -0300

    Correção de local de arquivo

--- trabalho_finalR.R
+++ trabalho_finalR.R
@@ -1,4 +1,4 @@
 library(tidyverse)
 
-df <- read.csv("pda-prouni-2019.csv", h = T, sep = ";", encoding = "UTF-8")
+df <- read.csv("data/pda-prouni-2019.csv", h = T, sep = ";", encoding = "UTF-8")
   #Base de dados a ser utilizada com informações de bolsistas do ProUni
